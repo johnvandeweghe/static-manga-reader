6768266e7b6d60f9e69b97ba3a0ac22d916243ad
John VanDeWeghe <vandeweghe@lunixlabs.com>
2018-02-07 00:41:15 -0600

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import { createRouter } from './router'
+
+const navbarComponent = () => import('./components/navbar').then(({ NavbarComponent }) => NavbarComponent)
+// const navbarComponent = () => import(/* webpackChunkName: 'navbar' */'./components/navbar').then(({ NavbarComponent }) => NavbarComponent)
+
+import './sass/main.scss'
+
+// tslint:disable-next-line:no-unused-expression
+new Vue({
+  el: '#app-main',
+  router: createRouter(),
+  components: {
+    'navbar': navbarComponent
+  }
+})
